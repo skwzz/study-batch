f5a86870c8a1e3adbd85af149565b1a011c454f7
KiwoongSon <skw9108@ibizsoftware.net>
2022-05-16 16:34:11 +0900

    예외 젠킨스 종료 테스트

--- src/main/java/com/example/studybatch/StudyBatchApplication.java
+++ src/main/java/com/example/studybatch/StudyBatchApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @Slf4j
 @EnableBatchProcessing
@@ -11,14 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyBatchApplication {
 
     public static void main(String[] args) {
-        int exitCode = 0;
-        try{
-            SpringApplication.run(StudyBatchApplication.class, args);
-        }catch(Exception e){
-            exitCode = 5;
-        }finally {
-            log.info("EXIT CODE : "+exitCode);
-        }
+        ConfigurableApplicationContext context = SpringApplication.run(StudyBatchApplication.class, args);
+        int exitCode = SpringApplication.exit(context);
+        log.info("EXIT CODE : " + exitCode);
     }
 
 }
